0dbc0f5606c03597eb7bd004611bc006247dd0ab
bayanbayan1996090-create <bayanbayan1996090@gmail.com>
2026-08-07 16:13:21 +0400

    Fix migration: merge new columns into init migration

--- prisma/migrations/add_worker_columns/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Worker" ADD COLUMN "directorate" TEXT,
-ADD COLUMN "department" TEXT,
-ADD COLUMN "office" TEXT,
-ADD COLUMN "location" TEXT,
-ADD COLUMN "shamCashAccount" TEXT;
